13ae8cc21cbaf0fb1fcc0729608123cfe297e22f
Erick Mangueira <erickmangueira@gmail.com>
2021-09-23 09:53:06 -0300

    Fix - better treatament to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,7 +25,7 @@ public class Program {
 				System.out.println();
 				System.out.print(" Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
@@ -38,12 +38,18 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				
+
 				if(chessMatch.getPromoted() != null) {
 					System.out.print(" Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.println(" Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+
+
+					}
 					chessMatch.replacePromotedPiece(type);
-					
+
 				}
 			}
 			catch (ChessException e) {
@@ -55,7 +61,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
-		
+
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
 	}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -54,7 +53,7 @@ public class ChessMatch {
 	public ChessPiece getEnPassantVulnerabe() {
 		return enPassantVulnerable;
 	}
-	
+
 	public ChessPiece getPromoted() {
 		return promoted;
 	}
@@ -87,7 +86,7 @@ public class ChessMatch {
 			throw new ChessException("You can't put yourself in check");
 		}
 		ChessPiece movedPiece = (ChessPiece)board.piece(target);
-		
+
 		//#special move promotion
 		promoted = null;
 		if ( movedPiece instanceof Pawn) {
@@ -96,7 +95,7 @@ public class ChessMatch {
 				promoted = replacePromotedPiece("Q");
 			}
 		}
-		
+
 
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 
@@ -117,14 +116,14 @@ public class ChessMatch {
 
 		return (ChessPiece)capturedPiece;
 	}
-	
-	
+
+
 	public ChessPiece replacePromotedPiece(String type) {
 		if(promoted == null) {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException(" Invalid type for promoted");
+			return promoted;
 		}
 		Position pos = promoted.getChessPosition().toPosition();
 		Piece p = board.removePiece(pos);
@@ -132,19 +131,19 @@ public class ChessMatch {
 		ChessPiece newPiece = newPiece(type, promoted.getColor());
 		board.placePiece(newPiece, pos);
 		piecesOnTheBoard.add(newPiece);
-		
-		
+
+
 		return newPiece;
-		
-		
-				
+
+
+
 	}
-	
+
 	private ChessPiece newPiece(String type, Color color) {
 		if( type.equals("B")) return new Bishop(board, color);
 		if( type.equals("N")) return new Knigth(board, color);
 		if( type.equals("Q")) return new Queen(board, color);
-		 return new Rook(board, color);
+		return new Rook(board, color);
 	}
 
 	private Piece makeMove(Position source, Position target) {
